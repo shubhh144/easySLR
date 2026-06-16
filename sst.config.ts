db051353140c4087f47cea8ab05ed9dd521ed6e4
shubh <moreshubham12340@gmail.com>
2026-06-16 11:53:41 +0530

    initial commit: EasySLR engineering assignment with custom favicon

---
+++ sst.config.ts
@@ -0,0 +1,22 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+export default $config({
+  app(input) {
+    return {
+      name: "easyslr",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    new sst.aws.Nextjs("MyWeb", {
+      environment: {
+        DATABASE_URL: process.env.DATABASE_URL!,
+        AUTH_SECRET: process.env.AUTH_SECRET!,
+        EMAIL_SERVER: process.env.EMAIL_SERVER!,
+        EMAIL_FROM: process.env.EMAIL_FROM!,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "",
+      }
+    });
+  },
+});
